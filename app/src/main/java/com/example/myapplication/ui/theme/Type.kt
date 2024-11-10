0035ccbde0a3e845e14a20bde41e93f951ae1f7f
Merhii <themerhi@hotmail.com>
2024-11-10 17:28:16 +0400

    signup page done

--- app/src/main/java/com/example/myapplication/ui/theme/Type.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Type.kt
@@ -2,9 +2,22 @@ package com.example.myapplication.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.myapplication.R
+
+//my popins
+val Poppins = FontFamily(
+//    Font(R.font.roboto_black, FontWeight.Black, FontStyle.Normal)
+        Font(R.font.poppins_regular,FontWeight.Black,FontStyle.Normal),
+    Font(R.font.poppins_bold,FontWeight.Black,FontStyle.Normal)
+//            Font(R.font.poppins_bold,FontWeight.Black,FontStyle.Normal)
+
+)
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
