3a73fadc109c329384aba360a50e23021b1c6b11
Merhii <themerhi@hotmail.com>
2024-12-08 01:24:13 +0200

    added device &automation

---
+++ app/src/main/java/com/example/myapplication/Entity/Automation.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.Entity
+
+data class Automation(
+    val automationid:Int,
+    val userid:Int,
+    val deviceid:Int,
+    val automationname:String,
+    val devicestatus:Int
+)
---
+++ app/src/main/java/com/example/myapplication/Entity/Device.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication.Entity
+
+data class Device (
+    val  deviceid: Int,
+    val devicename: String,
+    val devicelocation: String,
+    val devicestatus :Int
+    )
