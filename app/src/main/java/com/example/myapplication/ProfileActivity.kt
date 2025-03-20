e35b49ac53799536f47a70c2bce3d27ba817c88a
Merhii <themerhi@hotmail.com>
2025-03-20 22:52:56 +0200

    added logout

--- app/src/main/java/com/example/myapplication/ProfileActivity.kt
+++ app/src/main/java/com/example/myapplication/ProfileActivity.kt
@@ -1,4 +1,6 @@
 package com.example.myapplication
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
@@ -12,6 +14,22 @@ class ProfileActivity : ComponentActivity() {
         setContentView(R.layout.profile)
         val username = intent.getStringExtra("Name") ?: "Default Name"  // You can replace "Default Name" with any default value you prefer
         var name = findViewById<TextView>(R.id.name)
+        var logout = findViewById<TextView>(R.id.logout)
+
         name.text =  "$username"
+        logout.setOnClickListener{
+            logout()
+        }
+    }
+    private fun logout() {
+        // Clear the stored authentication data
+        val sharedPreferences = getSharedPreferences("MyAppPrefs", Context.MODE_PRIVATE)
+        sharedPreferences.edit().clear().apply()
+
+        // Redirect to the login activity
+        val intent = Intent(this, MainActivity::class.java)
+        // Clearing the activity stack and starting new task
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 }
